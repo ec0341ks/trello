18220c34c0a59c58834ae7e1aedfc44d87d2c3be
ec0341ks <idc68730@gmail.com>
2020-01-28 23:11:39 +0900

    ログイン機能の実装

--- trello/urls.py
+++ trello/urls.py
@@ -8,3 +8,8 @@ urlpatterns = [
     path("home/", views.home, name="home"),
     path("signup/", views.signup, name="signup"),
 ]
+
+# trello/urls.pyで定義したもの
+# trello/
+# trello/home/
+# trello/signup/
